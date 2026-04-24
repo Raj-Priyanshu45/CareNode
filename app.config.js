f2c8e23c7eb902d17211760b123e0e05e85e8430
Priyanshu Raj <devxpriyanshu@example.com>
2026-04-24 09:50:33 +0000

    f10

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = ({ config }) => ({
   ...config,
   extra: {
-    BASE_URL: process.env.BASE_URL || 'http://10.0.2.2:8080/api',
+    BASE_URL: process.env.BASE_URL || 'http://10.255.255.254:8080/api',
     AI_BASE_URL: process.env.AI_BASE_URL || 'http://10.0.2.2:8000',
   },
 });
